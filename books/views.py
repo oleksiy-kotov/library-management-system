825bde505802327e39ac7bfa56701ffc0c357bc1
Kotov Oleksiy <kotov.oleksiy@icloud.com>
2025-12-18 19:54:53 +0200

    Add permissions to the Books Service

--- books/views.py
+++ books/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -8,4 +11,11 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     search_fields = ("title", "author")
-    filter_fields = ("title", "author", "cover", "inventory")
+    filterset_fields = ("title", "author", "cover", "inventory")
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    authentication_classes = (JWTAuthentication,)
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return [permissions.AllowAny()]
+        return [permissions.IsAdminUser()]
